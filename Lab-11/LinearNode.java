48a2fd3009dd0e7ac3e70c7af878908ed98062c4
Ryan Posey <ryan.m.posey@gmail.com>
2025-05-03 18:30:50 -0600

    formating

--- Lab-11/LinearNode.java
+++ Lab-11/LinearNode.java
@@ -1,3 +1,4 @@
+
 /**
  * LinearNode represents a node in a linearly linked structure.
  *
@@ -5,69 +6,66 @@
  * @version 4.0
  */
 public class LinearNode<E> {
-	private LinearNode<E> next;
-	private E element;
 
-	/**
-  	 * Creates an empty node.
-  	 */
-	public LinearNode() {
-		next = null;
-		element = null;
-	}
+    private LinearNode<E> next;
+    private E element;
 
-	/**
-  	 * Creates a node storing the specified element.
- 	 *
-  	 * @param elem
-  	 *            the element to be stored within the new node
-  	 */
-	public LinearNode(E elem) {
-		next = null;
-		element = elem;
-	}
+    /**
+     * Creates an empty node.
+     */
+    public LinearNode() {
+        next = null;
+        element = null;
+    }
 
-	/**
- 	 * Returns the node that follows this one.
-  	 *
-  	 * @return the node that follows the current one
-  	 */
-	public LinearNode<E> getNext() {
-		return next;
-	}
+    /**
+     * Creates a node storing the specified element.
+     *
+     * @param elem the element to be stored within the new node
+     */
+    public LinearNode(E elem) {
+        next = null;
+        element = elem;
+    }
 
-	/**
- 	 * Sets the node that follows this one.
- 	 *
- 	 * @param node
- 	 *            the node to be set to follow the current one
- 	 */
-	public void setNext(LinearNode<E> node) {
-		next = node;
-	}
+    /**
+     * Returns the node that follows this one.
+     *
+     * @return the node that follows the current one
+     */
+    public LinearNode<E> getNext() {
+        return next;
+    }
 
-	/**
- 	 * Returns the element stored in this node.
- 	 *
- 	 * @return the element stored in this node
- 	 */
-	public E getElement() {
-		return element;
-	}
+    /**
+     * Sets the node that follows this one.
+     *
+     * @param node the node to be set to follow the current one
+     */
+    public void setNext(LinearNode<E> node) {
+        next = node;
+    }
 
-	/**
- 	 * Sets the element stored in this node.
-  	 *
-  	 * @param elem
-  	 *            the element to be stored in this node
-  	 */
-	public void setElement(E elem) {
-		element = elem;
-	}
+    /**
+     * Returns the element stored in this node.
+     *
+     * @return the element stored in this node
+     */
+    public E getElement() {
+        return element;
+    }
 
-	@Override
-	public String toString() {
-		return "Element: " + element.toString() + " Has next: " + (next != null);
-	}
+    /**
+     * Sets the element stored in this node.
+     *
+     * @param elem the element to be stored in this node
+     */
+    public void setElement(E elem) {
+        element = elem;
+    }
+
+    @Override
+    public String toString() {
+        return "Element: " + element.toString() + " Has next: " + (next != null);
+    }
 }
-
